0889ef33f9e6daded8616a06bdc42855bf5221ba
Marcel <bezdrighin@gmail.com>
2018-03-12 14:50:50 +0100

    Add comma separated tags.

--- archive/templatetags/filters.py
+++ archive/templatetags/filters.py
@@ -18,3 +18,14 @@ def get_item(dictionary, key):
 @register.filter
 def format_code(source):
 	return highlight(parse_string(source),PythonLexer(),HtmlFormatter())
+
+@register.filter
+def tags_to_comma_list(a):
+	if len(a)==0:
+		return ""
+
+	s = str(a[0].name)
+	for i in range(1,len(a)):
+		s+=", "+a[i].name
+
+	return s
